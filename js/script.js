158bfd386cbf4efed9b539f624a14b37ae265b9d
Obatola David Raymond <60965587+Raykingflamez@users.noreply.github.com>
2021-08-08 12:24:42 +0100

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-var open = document.getElementById('chatOpen')
-open.onclick = function chat_open(){
-    document.getElementById('chatBox').style.display = 'none';
-    document.getElementById('secondBox').style.opacity = '1';
-    document.getElementById('secondBox').style.transition = '300ms ease-in-out';
-}
